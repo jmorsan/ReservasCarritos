5d64dd9d96d396404c7eef2cdc96b3ac6abe7f05
jmorsan426 <jmorsan426@g.educaand.es>
2023-01-16 13:07:33 +0100

    Avance

---
+++ src/main/java/es/iesjandula/reservascarritos/repositories/IReservaCarritoPcsRepository.java
@@ -0,0 +1,14 @@
+package es.iesjandula.reservascarritos.repositories;
+
+import es.iesjandula.reservascarritos.models.CarritoPcs;
+import es.iesjandula.reservascarritos.models.ReservaCarritoPcsId;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author Joaquín Moreno
+ *
+ * ReservaCarritoPcs Repository
+ */
+public interface IReservaCarritoPcsRepository extends JpaRepository<CarritoPcs, ReservaCarritoPcsId>
+{
+}
