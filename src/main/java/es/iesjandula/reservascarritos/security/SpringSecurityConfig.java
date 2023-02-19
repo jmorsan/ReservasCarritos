8af6db76916acf1ae0d95f1c71f42be79752dbb2
jmorsan <105159568+jmorsan@users.noreply.github.com>
2023-02-19 19:02:29 +0100

    Security Fin

--- src/main/java/es/iesjandula/reservascarritos/security/SpringSecurityConfig.java
+++ src/main/java/es/iesjandula/reservascarritos/security/SpringSecurityConfig.java
@@ -8,6 +8,9 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+/**
+ * Class Spring Security Configuration
+ */
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig
@@ -19,9 +22,16 @@ public class SpringSecurityConfig
                 // /* -> Todos los endpoints en ese nivel. Ejemplo: /reservas , /mostrarAula,...
                 // /** -> Todos los endpoints y todos los de dentro de estos(recursivo). Ejemplo: /reservas/carritos/pcs...
 
-                .requestMatchers("/reservas/mostrar_reservas_carrito_tablets").hasRole("User_Carrito_Tablets")
-                .requestMatchers("/reservas/mostrar_reservas_carrito_pcs","/reservas/reservar_carrito_pcs").hasRole("User_Carrito_Pcs")
-                .requestMatchers("/**").hasRole("ADMIN")
+                //Ejemplo Varios Niveles de Seguridad:
+                //.requestMatchers("/reservas/mostrar_reservas_carrito_tablets").hasRole("User_Carrito_Tablets")
+                //.requestMatchers("/reservas/mostrar_reservas_carrito_pcs","/reservas/reservar_carrito_pcs").hasRole("User_Carrito_Pcs")
+                //.requestMatchers("/**").hasRole("ADMIN")
+
+
+                .requestMatchers("/reservas/mostrar_reservas_carrito_tablets","/reservas/reservar_carrito_tablets","/reservas/cancelar_reserva_carrito_pcs",
+                        "/reservas/mostrar_reservas_carrito_pcs","/reservas/reservar_carrito_pcs","/reservas/cancelar_reserva_carrito_pcs",
+                        "/reservas/mostrar_reservas_aula","/reservas/reservar_aula","/reservas/cancelar_reserva_aula",
+                        "/reservas/mostrar_reservas","/reservas/confirmar").hasAnyRole("ADMIN","User_Carrito_1Planta","User_Carrito_1Planta")
                 .and().formLogin()
                 .and().httpBasic();
 
@@ -29,13 +39,17 @@ public class SpringSecurityConfig
 
     }
 
+    /**
+     * Metodo controlador de usuarios y roles.
+     * @return los diferentes roles
+     */
     @Bean
     public InMemoryUserDetailsManager userDetailsManager()
     {
         return  new InMemoryUserDetailsManager(
                 User.withUsername("Admin").password("{noop}demo").roles("ADMIN").build(),
-                User.withUsername("User1").password("{noop}demo").roles("User_Carrito_Tablets").build(),
-                User.withUsername("User2").password("{noop}demo").roles("User_Carrito_Pcs").build()
+                User.withUsername("User1").password("{noop}demo").roles("User_Carrito_1Planta").build(),
+                User.withUsername("User2").password("{noop}demo").roles("User_Carrito_1Planta").build()
 
         );
     }
